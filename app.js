e1a48eb27249ea54c009b366be82b1b248acae70
Zukhriddin <zrruziev@gmail.com>
2021-07-08 20:29:27 +0900

    You can use Enter Button

--- app.js
+++ app.js
@@ -17,4 +17,19 @@ searchBtn.addEventListener('click', function() {
 
 
 
+searchInput.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13 || e.key === "Enter") {
+    e.preventDefault();
+    if(searchInput.value !== ''){
+      showProfile();
+      setTimeout(() => {
+        showRepos();
+      }, 600);
+  }
+  else {
+    showAlert('Enter a username', 'alert alert-danger');
+        clearProfile();
+  }
+  }
 
+});
